39c10b0f9d7005dab5d36f5d2ce07b38e0bb2e60
azeam <azeam>
2022-02-10 17:10:35 +0600

    added a file

---
+++ src/componens/PLayes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PLayes = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto deserunt dolor dolore harum itaque perspiciatis quae reprehenderit unde. Assumenda atque beatae ducimus et, nihil nulla obcaecati odio provident, quos, ratione rem reprehenderit veritatis voluptate. Consectetur ducimus in libero, nisi quos temporibus vel! Asperiores culpa cum, eos excepturi illo iste modi nostrum quibusdam vel? Accusamus, aliquam eaque fugiat id iste minima nihil nisi quae ullam veniam. Assumenda est itaque nobis provident rerum. Adipisci consectetur ex laboriosam nihil optio quibusdam repellendus velit? Dolore eius eligendi, hic ipsa magni nulla officiis quas quasi quibusdam recusandae, rem saepe suscipit voluptatem. A alias blanditiis quod.
+        </div>
+    );
+};
+
+export default PLayes;
